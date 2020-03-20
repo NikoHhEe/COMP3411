6940901a51db184b37ef828b9d289beb9acc64f7
NikoHhEe <m18888681190@163.com>
2020-03-20 17:23:15 +1100

    modify eval

--- Prolog/assign1_part1.pl
+++ Prolog/assign1_part1.pl
@@ -1,5 +1,6 @@
 % Zhichao He  z5282955 Assignment1-Prolog and Search
 
+% Q1.1
 % sumsq_even(Numbers, Sum)
 % Numbers: a list of integers 
 % Sum: the sum of all the squares of the even numbers in the list
@@ -22,6 +23,7 @@ sumsq_even([Head|Tail], Sum) :-
 
 
 
+% Q1.2
 % log_table(NumberList,ResultList) 
 % NumberList: a list of numbers
 % ResultList: a list of pairs consisting of a number and its log, 
@@ -40,6 +42,7 @@ log_table([Head|Tail], [[Head,Log]|ResultTail]) :-
 
 
 
+% Q1.3
 % paruns(List,RunList)
 % List: a list of integers
 % RunList: a list of list of integers where each list is a maximal
@@ -88,10 +91,13 @@ even(Num) :- integer(Num), 0 is Num mod 2.
 
 
 
+% Q1.4
 % eval(Expr, Val)
 % Expr: an arithmetic expression written in prefix format
 % Val: the final value of the expression
 
+% Base case 1: an atomic number 
+eval(Num, Num) :- number(Num).
 % Base case 2: operation on two atomic numbers
 % addition
 eval(add(Num1, Num2), Result) :- 
@@ -127,9 +133,6 @@ eval(mul(Expr1, Expr2), Result) :-
 eval(div(Expr1, Expr2), Result) :-
     evalEE(Expr1, Expr2, Result1, Result2),
     eval(div(Result1, Result2), Result).
-
-% Base case 1: an atomic number 
-eval(Num, Num) :- number(Num).
 
 % helper predicate to simplify the code 
 evalEE(Expr1, Expr2, Result1, Result2) :-
